6291d541c727f2680b7c2a0b961864d37bfa9910
SH <shajjad.hossain@etu.univ-lyon1.fr>
2020-12-24 15:55:01 +0100

    svelte app for data viz

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,44 @@
+<main>
+	<div id="app"></div>
+</main>
+
+
+<script>
+	import * as d3 from 'd3';
+	import { onMount } from 'svelte';
+
+	let width = 700, height = 580;
+
+	let bombusUrl = '/bombus_terrestris_freqs.csv';
+
+	// let euGeoJsonUrl = '/european-union-countries.geojson';
+	let euGeoJsonUrl = '/custom.geo.json'; // generate from 'https://geojson-maps.ash.ms/'
+	// have a look at https://datahub.io/core/geo-nuts-administrative-boundaries
+
+	var projection = d3
+		.geoConicConformal()
+		.center([11.454071, 47.279229])
+		.scale(1200);
+
+	let path = d3.geoPath().projection(projection);
+	let euGeoJson = d3.json(euGeoJsonUrl);
+
+
+	onMount(() => {
+		let svg = d3
+			.select("#app")
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height);
+	
+		euGeoJson.then(data => {
+			svg
+			.selectAll("path")
+			.data(data.features)
+			.enter()
+			.append('path')
+			.attr('d', path);
+			console.log(data);
+		});
+	})
+</script>
