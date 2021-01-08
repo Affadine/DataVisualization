9a6c9c334c30ce1b44e7844ecb51a43493eb11c8
HAMAT ABDOULAYE AFFADINE p2021446 <abdoulaye-affadine.hamat@etu.univ-lyon1.fr>
2021-01-08 16:41:51 +0100

    changement route.js

--- svelte-app/src/routes.js
+++ svelte-app/src/routes.js
@@ -4,6 +4,7 @@ import Home from './components/Home.svelte';
 import Carte from './components/Carte.svelte';
 //import TotalMap from './components/TotalMap.svelte';
 import TimeChart from './components/TimeChart.svelte';
+import horizontal_histo from './components/horizontal_histo.svelte';
 
 
 // let euGeoJsonUrl = 'https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson';
@@ -48,6 +49,14 @@ export default [
         'name': 'Analyse temporelle',
         'href': '/TimeChart',
         'component': TimeChart,
+        'ripos': ripos,
+        'navbar': true
+    },
+
+    {
+        'name': 'Horizontal Histo',
+        'href': '/HorizontalChat',
+        'component': horizontal_histo,
         'ripos': ripos,
         'navbar': true
     }
