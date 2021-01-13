0e9c500a0b1db9d40034d07421372ccb26247023
SH <shajjad.hossain@etu.univ-lyon1.fr>
2021-01-13 09:26:27 +0100

    home modif

--- svelte-app/src/components/Home.svelte
+++ svelte-app/src/components/Home.svelte
@@ -5,14 +5,14 @@
         <div class="col-3"></div>
         <div class="col-6">
             <p style="text-align: justify;">
-                In our project we visualized the evolution of bee species in Europe.
+                In our project we visualized the evolution of genus Bombus bee species in Europe.
                 We implemented four visualizations, showing the geographical distribution of data, the evolution with time and also a study by country on the 
                 proliferation of differents species of bees.
             </p>
             <p style="text-align: justify;" class="text-muted">
-                This project was done by: HOSSAIN Shajjad, BAMBA William, GLASS Philippe, HAMMAT Abdoulaye. Masters 2nd years students.
+                This project was done by: HOSSAIN Shajjad, BAMBA William, GLASS Philippe, HAMAT Abdoulaye. Masters 2nd years students.
             </p>
-            Source pour les données : http://www.atlashymenoptera.net/page.aspx?id=169
+            Data Source : <a href="http://www.atlashymenoptera.net/page.aspx?id=169" target="_blank">http://www.atlashymenoptera.net/page.aspx?id=169</a>
         </div>
         <div class="col-3"></div>
     </div>
